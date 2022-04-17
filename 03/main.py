255e0c47dffb68916b5a63d22cf5ef026ca5944b
Thorkil <schjodt.thorkil@gmail.com>
2022-04-17 20:35:42 +0200

    completed 03

--- 03/main.py
+++ 03/main.py
@@ -1,13 +1,14 @@
-import pprint
+from re import I
 
 
 def count_bits(input='input.txt'):
     fhand = open(input, "r")
-    amt_bits = [
-        {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0},
-        {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0},
-        {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0}
-        ]
+    # amt_bits = [
+    #     {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0},
+    #     {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0},
+    #     {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0}, {0: 0, 1: 0}
+    #     ]
+    amt_bits = [{0: 0, 1: 0} for i in range(12)]
     for line in fhand:
         line = line.rstrip()
         counter = 0
@@ -43,21 +44,76 @@ def consumption(gamma, epsilon):
 
 
 def calculate_energy():
-    a = count_bits()
-    gamma_binary = find_gamma(a)
+    bits = count_bits()
+    gamma_binary = find_gamma(bits)
     gamma = int(gamma_binary, 2)
-    epsilon_binary = find_epsilon(a)
+    epsilon_binary = find_epsilon(bits)
     epsilon = int(epsilon_binary, 2)
     cons = consumption(gamma, epsilon)
 
     print(f'Gamma: {gamma}, Epsilon: {epsilon}')
     print(f'Consumption: {cons}')
 
+# PART 2
+
+
+def input_to_list():
+    fhand = open('input.txt', 'r')
+    to_list = [line.rstrip() for line in fhand]
+    fhand.close()
+    return to_list
+
+
+def find_most(items, index):
+    bits = {0: 0, 1: 0}
+    for number in items:
+        bits[int(number[index])] += 1
+
+    return 1 if bits[1] >= bits[0] else 0
+
+
+def find_least(items, index):
+    bits = {0: 0, 1: 0}
+    for number in items:
+        bits[int(number[index])] += 1
+
+    return 0 if bits[0] <= bits[1] else 1
+
+
+def find_oxygen(report, index):
+    most = str(find_most(report, index))
+    keep = [x for x in report if x[index] == most]
+    return keep
+
+
+def find_co2(report, index):
+    least = str(find_least(report, index))
+    keep = [x for x in report if x[index] == least]
+    return keep
+
 
 def calculate_lifesupport():
-    bits = count_bits()
-    pprint.pprint(bits)
+    report = input_to_list()
+    counter = 0
+    while len(report) > 1:
+
+        report = find_oxygen(report, counter)
+        counter += 1
+
+    oxygen = int(report[0], 2)
+
+    report = input_to_list()
+    counter = 0
+    while len(report) > 1:
+
+        report = find_co2(report, counter)
+        counter += 1
+
+    co2 = int(report[0], 2)
+
+    print(oxygen * co2)
+
 
 
 calculate_lifesupport()
-calculate_energy()
+# calculate_energy()
